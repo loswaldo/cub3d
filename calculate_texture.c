d43da4c3e85b4059b903ef1bde4fa0a777915e63
loswaldo <shuriken_tears@gmail.com>
2021-03-12 02:09:34 +0300

    norm calc

--- calculate_texture.c
+++ calculate_texture.c
@@ -13,27 +13,27 @@
 #include "for_texture_n_sprites.h"
 #include "cub.h"
 
-void calc_side_dist(t_for_calculate *value, t_config *config)
+void	calc_side_dist(t_for_calculate *value, t_config *config)
 {
-	if(value->ray_dir_x < 0 && (value->step_x = -1))
+	if (value->ray_dir_x < 0 && (value->step_x = -1))
 	{
 		value->side_dist_x = (config->pl_pos_x - value->map_x)
-							 * value->delta_dist_x;
+							* value->delta_dist_x;
 	}
 	else if ((value->step_x = 1))
 	{
-		value->side_dist_x = (value->map_x + 1.0 - config->pl_pos_x)
-							 * value->delta_dist_x;
+		value->side_dist_x = (value->map_x + 1.f - config->pl_pos_x)
+							* value->delta_dist_x;
 	}
-	if(value->ray_dir_y < 0 && (value->step_y = -1))
+	if (value->ray_dir_y < 0 && (value->step_y = -1))
 	{
 		value->side_dist_y = (config->pl_pos_y - value->map_y)
-							 * value->delta_dist_y;
+							* value->delta_dist_y;
 	}
 	else if ((value->step_y = 1))
 	{
-		value->side_dist_y = (value->map_y + 1.0 - config->pl_pos_y)
-							 * value->delta_dist_y;
+		value->side_dist_y = (value->map_y + 1.f - config->pl_pos_y)
+							* value->delta_dist_y;
 	}
 }
 
@@ -57,12 +57,12 @@ void	calc_perpl_wall_dist(t_config *config, t_for_calculate *value)
 	if (value->side == 0)
 	{
 		value->perp_wall_dist = (value->map_x - config->pl_pos_x +
-								 (1 - value->step_x) / 2) / value->ray_dir_x;
+								(1.f - value->step_x) / 2) / value->ray_dir_x;
 	}
 	else
 	{
 		value->perp_wall_dist = (value->map_y - config->pl_pos_y +
-								 (1 - value->step_y) / 2) / value->ray_dir_y;
+								(1.f - value->step_y) / 2) / value->ray_dir_y;
 	}
 }
 
@@ -77,15 +77,15 @@ void	calc_draw_start_n_end(t_config *config, t_for_calculate *value, int x)
 	value->draw_end = value->line_height / 2 + config->Ry / 2;
 	if (value->draw_end >= config->Ry)
 		value->draw_end = config->Ry - 1;
-	if(value->side == 0)
+	if (value->side == 0)
 	{
 		value->wall_x = config->pl_pos_y + value->perp_wall_dist *
-										   value->ray_dir_y;
+										value->ray_dir_y;
 	}
 	else
 	{
 		value->wall_x = config->pl_pos_x + value->perp_wall_dist *
-										   value->ray_dir_x;
+										value->ray_dir_x;
 	}
 	value->wall_x -= floor((value->wall_x));
 }
@@ -107,11 +107,11 @@ void	check_side_n_tex_x_y(t_config *config, t_for_calculate *value)
 						config->EA_tex.height;
 	}
 	value->tex_x = (int)(value->wall_x * (double)(config->tmp_w));
-	if(value->side == 0 && value->ray_dir_x > 0)
+	if (value->side == 0 && value->ray_dir_x > 0)
 		value->tex_x = config->tmp_w - value->tex_x - 1;
-	if(value->side == 1 && value->ray_dir_y < 0)
+	if (value->side == 1 && value->ray_dir_y < 0)
 		value->tex_x = config->tmp_w - value->tex_x - 1;
 	value->step = 1.0 * config->tmp_h / value->line_height;
 	value->tex_pos = (value->draw_start - config->Ry
-										  / 2 + value->line_height / 2) * value->step;
+						/ 2 + value->line_height / 2) * value->step;
 }
