24944cd193d93876e913c368c39066742c85b83f
loswaldo <shuriken_tears@gmail.com>
2021-03-12 03:14:09 +0300

    norme key hooks

--- key_hooks.c
+++ key_hooks.c
@@ -12,56 +12,51 @@
 
 #include "cub.h"
 
+#define MOVE_S 0.2f
+
+void	move_up_down(float sign, t_config *config)
+{
+	const int	x_map_row = config->pl_pos_x + config->dir_x * MOVE_S * sign;
+	const int	x_map_column = config->pl_pos_y;
+	const int	y_map_row = config->pl_pos_x;
+	const int	y_map_column = config->pl_pos_y + config->dir_y * MOVE_S * sign;
+
+	if (config->map[x_map_row][x_map_column] == '0')
+		config->pl_pos_x += config->dir_x * MOVE_S * sign;
+	if (config->map[y_map_row][y_map_column] == '0')
+		config->pl_pos_y += config->dir_y * MOVE_S * sign;
+}
+
+void	move_right_left(float sign, t_config *config)
+{
+	const int	x_map_row = config->pl_pos_x;
+	const int	x_map_column = config->pl_pos_y + config->dir_x * MOVE_S * sign;
+	const int	y_map_row = config->pl_pos_x - config->dir_y * MOVE_S * sign;
+	const int	y_map_column = config->pl_pos_y;
+
+	if (config->map[x_map_row][x_map_column] == '0')
+		config->pl_pos_y += config->dir_x * MOVE_S * sign;
+	if (config->map[y_map_row][y_map_column] == '0')
+		config->pl_pos_x -= config->dir_y * MOVE_S * sign;
+}
+
 void	key_hook_move(int key, t_config *config)
 {
-	float	move_s;
-	float	move_r;
-
-	move_s = 0.2f;
-	move_r = 0.2f;
-
 	if (key == 13)
 	{
-		if (config->map[(int)(config->pl_pos_x
-							  + config->dir_x * move_s)]
-			[(int)(config->pl_pos_y)] == '0')
-			config->pl_pos_x += config->dir_x * move_s;
-		if (config->map[(int)(config->pl_pos_x)][(int)(config->pl_pos_y
-													   + config->dir_y * move_s)] == '0')
-			config->pl_pos_y += config->dir_y * move_s;
+		move_up_down(1, config);
 	}
-	if (key == 1)
+	else if (key == 1)
 	{
-		if (config->map[(int)(config->pl_pos_x
-							  - config->dir_x * move_s)]
-			[(int)(config->pl_pos_y)] == '0')
-			config->pl_pos_x -= config->dir_x * move_s;
-		if (config->map[(int)(config->pl_pos_x)]
-			[(int)(config->pl_pos_y
-				   - config->dir_y * move_s)] == '0')
-			config->pl_pos_y -= config->dir_y * move_s;
+		move_up_down(-1, config);
 	}
 	if (key == 0)
 	{
-		if (config->map[(int)(config->pl_pos_x)]
-			[(int)(config->pl_pos_y
-				   + config->dir_x * move_s)] == '0')
-			config->pl_pos_y += config->dir_x * move_s;
-		if (config->map[(int)(config->pl_pos_x
-							  - config->dir_y * move_s)]
-			[(int)(config->pl_pos_y)] == '0')
-			config->pl_pos_x -= config->dir_y * move_s;
+		move_right_left(1, config);
 	}
 	if (key == 2)
 	{
-		if (config->map[(int)(config->pl_pos_x)]
-			[(int)(config->pl_pos_y
-				   - config->dir_x * move_s)] == '0')
-			config->pl_pos_y -= config->dir_x * move_s;
-		if (config->map[(int)(config->pl_pos_x
-							  + config->dir_y * move_s)]
-			[(int)(config->pl_pos_y)] == '0')
-			config->pl_pos_x += config->dir_y * move_s;
+		move_right_left(-1, config);
 	}
 }
 
@@ -71,21 +66,21 @@ void	key_hook_rotation(int key, t_config *config)
 	float	o_dirx;
 	float	o_planex;
 
-	rot_s = (float)0.1;
+	rot_s = 0.1f;
 	if (key == 124 || key == 123)
 	{
 		if (key == 123)
 			rot_s *= -1;
 		o_dirx = config->dir_x;
 		config->dir_x = config->dir_x * cosf(-rot_s) - config->dir_y *
-													   sinf(-rot_s);
+						sinf(-rot_s);
 		config->dir_y = o_dirx * sinf(-rot_s) + config->dir_y *
-												cosf(-rot_s);
+						cosf(-rot_s);
 		o_planex = config->plane_x;
 		config->plane_x = config->plane_x * cosf(-rot_s)
-						  - config->plane_y * sinf(-rot_s);
+						- config->plane_y * sinf(-rot_s);
 		config->plane_y = o_planex * sinf(-rot_s) + config->plane_y
-													* cosf(-rot_s);
+							* cosf(-rot_s);
 	}
 }
 
@@ -100,7 +95,7 @@ int		key_hook(int key, t_config *config)
 	mlx_destroy_image(config->win->mlx, config->win->img);
 	config->win->img = mlx_new_image(config->win->mlx, config->rx, config->ry);
 	config->win->addr = mlx_get_data_addr(config->win->img, &config->win->bpp,
-										  &config->win->line_len, &config->win->endian);
+						&config->win->line_len, &config->win->endian);
 	draw_map(config);
 	mlx_put_image_to_window(config->win->mlx, config->win->mlx_win,
 							config->win->img, 0, 0);
